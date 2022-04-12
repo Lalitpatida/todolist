a8a480ce090b8c0dca0eb1068e2507081a7429f5
Lalitpatida <96428561+Lalitpatida@users.noreply.github.com>
2022-04-12 20:09:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header'
+import TodoItems from './components/TodoItems'
+import TodoControls from './components/TodoControls'
+
+function App() {
+
+  const [items, setItems] = useState([]);
+  console.log(items);
+
+  function addTodoItem(item) {
+    setItems(prev => [...prev, item]);
+  }
+function toggleComplete(todoIndex) {
+    setItems(items.map((item, i) => {
+        return i === todoIndex ? { ...item, complete: !item.complete } : item;
+      }
+    ));
+  }
+function removeTodo(todoIndex) {
+    let filtered = items.filter((_item, i) => i !== todoIndex);
+    console.log(filtered)
+    setItems(filtered);
+  }
+ return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <TodoItems items={items} toggleComplete={toggleComplete} removeTodo={removeTodo}/>
+        <TodoControls addTodoItem={addTodoItem} />
+      </div>
+    </div>
+  );
+}
+export default App;
